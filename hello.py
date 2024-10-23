3f764a8ff557ff2389ff99630e29807cec638cbe
Austin Graham <austiingraham@gmail.com>
2024-10-23 00:02:15 -0400

    update Hello World script with additional output and comments

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello World!") 
+print("Hello World!") # Output "Hello World!" to the console
+print("This is a Python program.")
+print("It doesn't do much, but it's a start.")
+print("I'll be learning more about Python soon.")
+print("I'm excited about it!")
+
+# Multi-line statement using a single print statement with \n
+print("Hello World! \nThis is a Python program. \nIt doesn't do much, but it's a start. \nI'll be learning more about Python soon. \nI'm excited about it!") 
+
+'''
+This is a
+multi-line
+comment
+'''
