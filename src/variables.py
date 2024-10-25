d60e4d2654c370332ba3eaf0ea84a7dc07d772ee
Austin Graham <austiingraham@gmail.com>
2024-10-25 00:21:49 -0400

    add variables.py with basic variable operations and string formatting

---
+++ src/variables.py
@@ -0,0 +1,21 @@
+my_name = "Austin Graham"
+print(my_name)
+
+number_1 = 5
+number_2 = 10
+print(number_1 + number_2)
+
+fruit_1 = "apples"
+fruit_2 = "oranges"
+print(fruit_1 + fruit_2)
+
+fruit_1 = "apples"
+fruit_2 = "oranges"
+print("I like " + fruit_1 + " and I like " + fruit_2 + ".")
+
+fruit_1 = "apples"
+fruit_2 = "oranges"
+print(f"I like {fruit_1} and I like {fruit_2}.")
+
+fruit_1 = "apples"
+print(fruit_1 * 5)
