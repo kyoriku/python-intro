355bcc61412a2fb9f48cc8ee720595a8fbfe637d
Austin Graham <austiingraham@gmail.com>
2024-10-25 00:24:37 -0400

    add comments to variables.py

--- src/variables.py
+++ src/variables.py
@@ -1,21 +1,35 @@
+# This script demonstrates basic variable assignments and operations in Python.
+
+# Assign a string to the variable 'my_name'
 my_name = "Austin Graham"
+# Print the value of 'my_name'
 print(my_name)
 
+# Assign integers to the variables 'number_1' and 'number_2'
 number_1 = 5
 number_2 = 10
+# Print the sum of 'number_1' and 'number_2'
 print(number_1 + number_2)
 
+# Assign strings to the variables 'fruit_1' and 'fruit_2'
 fruit_1 = "apples"
 fruit_2 = "oranges"
+# Concatenate and print 'fruit_1' and 'fruit_2'
 print(fruit_1 + fruit_2)
 
+# Reassign strings to 'fruit_1' and 'fruit_2'
 fruit_1 = "apples"
 fruit_2 = "oranges"
+# Print a sentence using string concatenation
 print("I like " + fruit_1 + " and I like " + fruit_2 + ".")
 
+# Reassign strings to 'fruit_1' and 'fruit_2'
 fruit_1 = "apples"
 fruit_2 = "oranges"
+# Print a sentence using an f-string
 print(f"I like {fruit_1} and I like {fruit_2}.")
 
+# Reassign a string to 'fruit_1'
 fruit_1 = "apples"
+# Print 'fruit_1' repeated 5 times
 print(fruit_1 * 5)
