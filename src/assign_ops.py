ee8a723ca829207d3b37b74cff46922b7fa67276
Austin Graham <austiingraham@gmail.com>
2024-10-26 00:45:11 -0400

    add comments to assign_ops.py

--- src/assign_ops.py
+++ src/assign_ops.py
@@ -8,15 +8,15 @@
 **= Exponentiates and assigns
 '''
 
-number_1 = 14
-number_1 = number_1 + 27
-print(number_1)
+number_1 = 14 # Assign 14 to number_1
+number_1 = number_1 + 27 # Add 27 to number_1 and assign the result back to number_1
+print(number_1)  # Output: 41
 
-number_1 = 14
-number_1 += 27
-print(number_1)
+number_1 = 14 # Assign 14 to number_1
+number_1 += 27 # Add 27 to number_1 using the += operator and assign the result back to number_1
+print(number_1)  # Output: 41
 
-number_1 = 14
-number_2 = 27
-number_1 += number_2
-print(number_1)
+number_1 = 14 # Assign 14 to number_1
+number_2 = 27 # Assign 27 to number_2
+number_1 += number_2 # Add number_2 to number_1 using the += operator and assign the result back to number_1
+print(number_1)  # Output: 41
