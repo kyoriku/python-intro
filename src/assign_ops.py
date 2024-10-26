bc2dbe9e5b0b01948d75773f74c7f5e2ac2dbf39
Austin Graham <austiingraham@gmail.com>
2024-10-26 00:27:50 -0400

    add assign_ops.py with examples of assignment operators

---
+++ src/assign_ops.py
@@ -0,0 +1,22 @@
+'''
+= Assigns something from the right to the left
++= Adds and assigns
+-= Subtracts and assigns
+*= Multiplies and assigns
+/= Divides and assigns
+%= Modulus and assigns
+**= Exponentiates and assigns
+'''
+
+number_1 = 14
+number_1 = number_1 + 27
+print(number_1)
+
+number_1 = 14
+number_1 += 27
+print(number_1)
+
+number_1 = 14
+number_2 = 27
+number_1 += number_2
+print(number_1)
