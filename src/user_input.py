119915c307a075c42f09e61a45a45d857976b4d4
Austin Graham <austiingraham@gmail.com>
2024-10-27 00:47:33 -0400

    add user_input.py with examples of user input handling

---
+++ src/user_input.py
@@ -0,0 +1,25 @@
+name = input("What's your name? ")
+print(f"Hello {name}")
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print(f"{num1} + {num2} = {int(num1)+int(num2)}")
+
+name = "Austin"
+print(f"Hello {name}")
+
+name = "Austin"
+print(name * 10)
+
+apples = input("How many apples would you like to purchase? ")
+print(f"You would like to purchase {apples} apples.")
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(f"Hello {first_name} {last_name}")
+
+name = input("What is your name? ")
+location = input("Where do you live? ")
+phone_number = input("What is your phone number? ")
+favorite_color = input("What is your favorite color? ")
+print(f"Name: {name}\nLocation: {location}\nPhone Number: {phone_number}\nFavorite Color: {favorite_color}")
